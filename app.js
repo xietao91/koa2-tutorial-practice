2731995dc3f5e2a84b10ebd976aa3c591aab73a8
xietao <931864989@qq.com>
2019-07-15 20:52:18 +0800

    更新中间件使用方法

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 // console.log('hello world');
-const Koa = require('koa');
+const Koa = require("koa");
 const app = new Koa();
-app.use(async (ctx, next)=>{
-    await next();
-    ctx.response.type = 'text/html';
-    ctx.response.body = '<h1>Hello World</h1>'
+
+// 记录执行世间
+app.use(async (ctx, next) => {
+  let stime = new Date().getTime();
+  await next();
+  let etime = new Date().getTime();
+  ctx.response.type = "text/html";
+  ctx.response.body = "<h1>Hello World</h1>";
+  console.log(`请求地址${ctx.path}, 响应时间: ${etime - stime}ms`);
 });
-app.listen(3000,()=>{
-    console.log('服务器运行在 localhost://3000');
+
+app.use(async (ctx, next) => {
+  console.log("中间件1 开始");
+  await next();
+  console.log("中间件1 结束");
+});
+
+app.use(async (ctx, next) => {
+  console.log("中间件2 开始");
+  await next();
+  console.log("中间件2 结束");
+});
+
+app.use(async (ctx, next) => {
+  console.log("中间件3 开始");
+  await next();
+  console.log("中间件3 结束");
+});
+
+app.listen(3000, () => {
+  console.log("服务器运行在 localhost://3000");
 });
