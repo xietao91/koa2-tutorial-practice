d6879299b3877b361e0df48ccff5317475cd2f4a
xietao <931864989@qq.com>
2019-07-23 22:52:27 +0800

    新增mi-log分支

--- code/middleware/index.js
+++ code/middleware/index.js
@@ -3,8 +3,10 @@ const bodyParser = require('koa-bodyparser');
 const nunjucks = require('koa-nunjucks-2');
 const staticFiles = require('koa-static');
 const miSend = require('./mi-send');
+const miLog = require('./mi-log');
 
 module.exports = (app) => {
+    app.use(miLog());
     app.use(miSend());
     // 指定public为静态资源目录,用于存放js,css,image等文件
     app.use(staticFiles(path.resolve(__dirname, '../public')));
